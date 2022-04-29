30e4e28469d3924ade945427c0e5337c32fc0296
Alexey Peresypkin <aepre3@gmail.com>
2022-04-29 07:18:18 +0300

    Created admin handler

--- create_bot.py
+++ create_bot.py
@@ -2,9 +2,13 @@ import os
 
 from aiogram import Bot, Dispatcher, executor
 from dotenv import load_dotenv
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 load_dotenv()
 bot_token = os.getenv('TELEGRAM_TOKEN')
 admin_id = os.getenv('ADMIN_ID')
+
+storage = MemoryStorage()
+
 bot = Bot(bot_token)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
--- main.py
+++ main.py
@@ -10,5 +10,7 @@ async def on_startup(_):
 
 if __name__ == '__main__':
     client.register_handlers_client(dp)
+    admin.register_handlers_admin(dp)
     other.register_handlers_other(dp)
+
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
